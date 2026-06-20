f74e15f2e8057eeb8248a202257245bfe026d1b0
Sagar Dabhole <SagarDabhole@gmail.com>
2026-06-20 12:35:18 +0530

    Almost most of the assignments for first course.

---
+++ 3/ta/pthread_128.c
@@ -0,0 +1,76 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sched.h>
+
+#include <syslog.h> // to print the sys logs
+
+#define TH_COUNT  128      // we need to create 128 threads
+
+// pThread structure
+typedef struct
+{
+    int threadIdx;
+} threadParams_t;
+
+
+// POSIX thread declarations and scheduling attributes
+//
+pthread_t threads[TH_COUNT];        // 1 each thread
+threadParams_t threadParams[TH_COUNT];  // 1 each thread
+
+
+
+void *eachThread(void *threadp)
+{
+    int i=0;
+    int gsum = 0;
+
+    threadParams_t* threadParams = (threadParams_t *)threadp;
+
+    for(i=0; i<threadParams->threadIdx; i++)
+    {
+        gsum=gsum+i;
+    }
+
+    // syslog message 
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT: 3]: Thread idx=%d, sum[1...%d] =%d\n", threadParams->threadIdx, threadParams->threadIdx, gsum);
+
+}
+
+
+int main (int argc, char *argv[])
+{
+   int i=0;
+
+    // syslog message 
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT: 3]: Create 128 Threads\n");
+
+   for(i=0;i<TH_COUNT;i++)
+   {
+        threadParams[i].threadIdx=i;
+
+        pthread_create(&threads[i],   // pointer to thread descriptor
+                (void *)0,     // use default attributes
+                eachThread, // thread function entry point
+                (void *)&(threadParams[i]) // parameters to pass in
+                );
+
+   }
+   
+    // syslog message 
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT: 3]: Join all the 128 Threads\n");
+
+    // Join all the threds 
+	for (i = 0; i < TH_COUNT; i++)
+	{
+		pthread_join(threads[i], NULL);
+	}
+
+    // syslog message 
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT: 3]: Assignment Finished.\n");
+
+   printf("Assignment Complete. \n");
+
+   closelog();
+}
